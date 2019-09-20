e936ecbd3efa733031b7dab64e7659919d9d49ee
Sagar DB (DropCodes) <sagardb50@gmail.com>
2019-09-20 19:24:32 +0530

    Created InvertHalfPyramid

---
+++ src/InvertHalfPyramid.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class InvertHalfPyramid {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		for(int i = x ; i>0;i--) {
+			for(int j = i;j>0;j--) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+/*		input 6
+ * 
+ * 		* * * * * *
+ * 		* * * * *
+ * 		* * * *
+ * 		* * *
+ * 		* *
+ * 		* 
+ * 
+ */
