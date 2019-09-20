541cf4ad23a16c1ab25400cf8fb9da5bbffbacc3
Sagar DB (DropCodes) <sagardb50@gmail.com>
2019-09-20 05:50:33 +0530

    Half Pyramid

---
+++ src/HalfPyramid.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class HalfPyramid {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int num = s.nextInt();
+		
+		for(int i = 0;i<=num;i++) {
+			for(int j = 1;j<=i;j++) {
+				System.out.print(j); // for * System.out.print(" * ");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
+
+
+/* 			input: 5
+ * 
+ * 			output
+ * 			1
+ * 			12
+ * 			123
+ * 			1234
+ * 			12345
+  */
