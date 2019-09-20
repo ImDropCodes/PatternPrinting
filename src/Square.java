22416f305ba2b536b1189416596001ba1220bb8a
Sagar DB (DropCodes) <sagardb50@gmail.com>
2019-09-20 05:59:50 +0530

    Create Square.java

---
+++ src/Square.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Square {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		for(int i = 1 ;i<=x;i++) {
+			for(int j = 1; j<=x;j++) {
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
+
+/*
+ * 			output
+ *			* * * * * 
+ *			* * * * *
+ *			* * * * *
+ *			* * * * *
+ *			* * * * *
+ *
+ */			
