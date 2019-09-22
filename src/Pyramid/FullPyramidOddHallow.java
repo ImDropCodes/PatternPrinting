f3348c998712aec3533fe36ae1797eba7d81a86f
Sagar DB (DropCodes) <sagardb50@gmail.com>
2019-09-22 09:06:15 +0530

    Update FullPyramidOddHallow.java

--- src/Pyramid/FullPyramidOddHallow.java
+++ src/Pyramid/FullPyramidOddHallow.java
@@ -9,15 +9,17 @@ public class FullPyramidOddHallow {
 		int x = s.nextInt();
 		
 		for(int i = 1; i<=x;++i) {
-			for(int j = 1;j<=2*x-1;++j) {
-				if(i==1 && j==x) {
-					System.out.print("*");
-				}
-				if(i!=1 && (j==x-i || j==x+i) || i==x) {
-					System.out.print("*");
-				}
-				else {
-					System.out.print(" ");
+			
+			for(int j = i;j<x;j++) {
+				System.out.print("  ");
+			}
+			for(int k = 1;k<=(2*i-1);k++) {
+				
+				if(k==(2*i-1) || k==1 || i==x) {
+					System.out.print("* ");
+					
+				}else {
+					System.out.print("  ");
 				}
 			}
 			
@@ -27,3 +29,20 @@ public class FullPyramidOddHallow {
 	}
 
 }
+
+
+/*
+ 
+	input 5
+	
+        * 
+      *   * 
+    *       * 
+  *           * 
+* * * * * * * * * 
+
+
+
+
+
+*/
